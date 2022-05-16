f359176eb017c7e08bb0e89e192e43cadd99e82e
Kwibuka Confiance <swiftmotion2020@gmail.com>
2022-05-16 11:18:38 +0200

    challenges done

---
+++ app.js
@@ -0,0 +1,67 @@
+// console.log("Hello Tech up skill");
+const print = (args) => {
+  return console.log(args);
+};
+// Syntax
+// How to use Variables
+//operators
+// datatypes
+//functions
+// conditions
+// loops
+//objects
+//arrays
+
+// var name, othername;
+// var name;
+// var othername;
+
+// let name;
+
+// constant variable;
+// const othername = 12;
+
+// let firstNumber = 12;
+// const lastNumber = 13;
+
+// firstNumber = 20;
+
+// const result = firstNumber - lastNumber;
+// console.log(result);
+
+// const addTwoNumbers = ()=>{};
+// const addTwoNumbers = function (){};
+// function addTwoNumbers (){}
+
+// if()
+
+// Q1 Display "You can watch adult film" if youre 20 and above years old or "youre to young to watch this films" if below 20 yars
+
+// function addTwoNumbers(number1, number2) {
+//   let result = number1 + number2;
+//   return result;
+// }
+
+// console.log(addTwoNumbers(32, 40));
+// forloop
+// while loop
+
+// let number = 100;
+
+// for (let i = 0; i <= 100; i++) {
+//   print(i);
+// }
+
+// function canIwatchAdultFilm(years, name) {
+//   // `Hello ${name}, You can watch adult film`
+//   if (years >= 20) return " hello " + name + " You can watch adult film";
+//   else return ` oh sorry ${name}, you're to young to watch this films`;
+// }
+
+// // console.log(canIwatchAdultFilm(20, "Shumbusho"));
+
+for (let i = 0; i <= 100; i++) {
+  if (i >= 50 && i <= 80) {
+    print(i);
+  }
+}
