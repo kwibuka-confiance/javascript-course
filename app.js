45f2dffa91975455e8e50c8f7e3af9cb0f2f3e2c
Kwibuka Confiance <swiftmotion2020@gmail.com>
2022-05-20 10:19:21 +0200

    add question

--- app.js
+++ app.js
@@ -83,4 +83,74 @@ const participants = [
 const result = participants.filter(
   (element) => element.cameTme == 11 && element.age == 21
 );
-console.log(result);
+
+// TODO JAVASCRIPT METHODS
+// filter
+// REDUCE
+
+const employees = [
+  { name: "Shumbusho", image: "", salary: 400, age: 22 },
+  { name: "Patrick", image: "", salary: 230, age: 24 },
+  {
+    name: "Alvin",
+    image:
+      "https://media.istockphoto.com/photos/passport-photo-of-serious-brazilian-young-adult-man-with-braces-picture-id1315808211?k=20&m=1315808211&s=612x612&w=0&h=VmBH7yrF7EQqPVQu9wAfge6opadEbvJf3OpOX9PARPg=",
+    salary: 250,
+    age: 22,
+  },
+  {
+    name: "Liliane",
+    image: "https://visafoto.com/img/docs/ee_passport_40x50mm.jpg",
+    salary: 200,
+    age: 22,
+  },
+  {
+    name: "Heritier",
+    image:
+      "https://bruinlife.s3.us-west-1.amazonaws.com/wp-content/uploads/2018/05/02172507/2B8_5802.jpg",
+    salary: 230,
+    age: 24,
+  },
+  { name: "Grace", image: "", salary: 300, age: 21 },
+  { name: "Gloria", image: "", salary: 200, age: 22 },
+];
+
+function findEmployeesBetween23KAnd30K(employeesArray) {
+  const filteredArray = employeesArray.filter(
+    (element, index) => element.salary >= 200 && element.salary <= 300
+  );
+  return filteredArray;
+}
+
+// SHow us the total salary to be paid
+
+const showAllSalaries = (allEmployees) => {
+  const totalSalary = allEmployees.reduce((acc, obj) => {
+    return obj.salary + acc;
+  }, 0);
+  return totalSalary;
+};
+
+// const finalresult = showAllSalaries(employees);
+// console.log(` TOTAL SALARY IS : ${finalresult}k`);
+
+// TODO DISPLAY THE ALL LIST WITH IMAGES BUT IF WE HAVE  IMAGE REPLACE IMAGE WITH "IMAGE",
+// IF WE FOUND NO IMAGE DISPLAY FIRST CHARACTER OF OUR NAME
+
+function displayList(listArray) {
+  const finalResult = listArray.map((element, index) => {
+    const firstChr = element.name.slice(0, 1).toUpperCase();
+    if (element.image === "") {
+      return `Number ${index + 1} ${firstChr} but name is ${
+        element.name
+      } with ${element.salary}K monthly `;
+    } else {
+      return `Number ${index + 1} has image and name is ${element.name} with ${
+        element.salary
+      }K monthly `;
+    }
+  });
+  return finalResult;
+}
+
+console.log(displayList(employees));
---
+++ practice.js
@@ -0,0 +1,83 @@
+movies = [
+  {
+    name: "Avengers",
+    type: "Sequel",
+    stars: 4.9,
+    boxoffice: "26",
+    cast: [
+      { name: "Chris Evans" },
+      { name: "Benedict Cumberbach" },
+      { name: "oody Harrelson" },
+      { name: "Mark Rufalo" },
+      { name: "Chris Hermworth" },
+      { name: "Chris Pratt" },
+      { name: "Andy Serkis" },
+    ],
+  },
+  {
+    name: "pirate of caribbean",
+    type: "Sequel",
+    stars: 4.9,
+    boxoffice: "18",
+    cast: [
+      { name: "Johnny Depp" },
+      { name: "Orlando Bloom" },
+      { name: "Kierra Knightley" },
+      { name: "Geofrey Right" },
+      { name: "Jonathan Pryce" },
+    ],
+  },
+  {
+    name: "pirate of caribbean",
+    type: "Sequel",
+    stars: 4.9,
+    boxoffice: "18",
+    cast: [
+      { name: "Johnny Depp" },
+      { name: "Orlando Bloom" },
+      { name: "Kierra Knightley" },
+      { name: "Geofrey Right" },
+      { name: "Jonathan Pryce" },
+    ],
+  },
+  {
+    name: "Lords of the ring",
+    type: "Sequel",
+    stars: 4.3,
+    boxoffice: "18",
+    cast: [
+      { name: "Andy Serkis" },
+      { name: "Orlando Bloom" },
+      { name: "Elijiah Wood" },
+      { name: "Karl Urban" },
+    ],
+  },
+  {
+    name: "Planet of the apes",
+    type: "Sequel",
+    stars: 4.6,
+    boxoffice: "18",
+    cast: [
+      { name: "Andy Serkis" },
+      { name: "Woody Harrelson" },
+      { name: "Tobby Kebell" },
+    ],
+  },
+  {
+    name: "Planet of the apes",
+    type: "Sequel",
+    stars: 4.6,
+    boxoffice: "18",
+    cast: [
+      { name: "Andy Serkis" },
+      { name: "Woody Harrelson" },
+      { name: "Tobby Kebell" },
+    ],
+  },
+];
+
+//  NB BOX OFFICE VALUE IS IN BILLION  YOU HAVE TO RETURN SUM IN BILLION
+
+//  TODO 1 WRITE JAVASCRIPT FUNCTION THAT HELP REMOVE DUPLICATED MOVIES;
+//  TODO 2 WRITE JAVASCRIPT FUNCTION THAT DISPLAY CHARACTERS PLAY IN 2 OR MORE MOVIES
+//  TODO 3 WRITE JAVASCRIPT FUNCTION THAT GIT THE SUM OR TOTAL OF ALL BOX OFFICE
